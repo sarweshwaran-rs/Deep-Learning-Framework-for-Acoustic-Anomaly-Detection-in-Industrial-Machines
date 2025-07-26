9b77b3f284a48808f256effe6e6d7459229389c4
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-26 16:05:47 +0530

    Update CNN encoder to support multi-channel spectrogram input

--- models/cnn_encoders.py
+++ models/cnn_encoders.py
@@ -5,7 +5,7 @@ from torchvision.models import ResNet18_Weights, EfficientNet_B0_Weights
 
 class BasicSpectrogramClassifier(nn.Module):
 
-    def __init__(self, encoder_name = 'resnet18', pretrained=False, num_classes=2):
+    def __init__(self, encoder_name = 'resnet18', pretrained=False, num_classes=2,in_channels=3):
         super(BasicSpectrogramClassifier, self).__init__()
 
         if encoder_name == 'resnet18':
@@ -14,7 +14,7 @@ class BasicSpectrogramClassifier(nn.Module):
             else:
                 self.encoder = models.resnet18(weights=None)
                 
-            self.encoder.conv1 = nn.Conv2d(1,64,kernel_size=7,stride=2,padding=3,bias=False)
+            self.encoder.conv1 = nn.Conv2d(in_channels,64,kernel_size=7,stride=2,padding=3,bias=False)
             num_ftrs = self.encoder.fc.in_features
             self.encoder.fc = nn.Identity() # type: ignore
         
@@ -23,7 +23,7 @@ class BasicSpectrogramClassifier(nn.Module):
                 self.encoder = models.efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
             else:
                 self.encoder = models.efficientnet_b0(weights=None)
-            self.encoder.features[0][0] = nn.Conv2d(1,32,kernel_size=3,stride=2, padding=1, bias=False)
+            self.encoder.features[0][0] = nn.Conv2d(in_channels,32,kernel_size=3,stride=2, padding=1, bias=False)
             num_ftrs = self.encoder.classifier[1].in_features
             self.encoder.classifier = nn.Identity() # type: ignore
 
