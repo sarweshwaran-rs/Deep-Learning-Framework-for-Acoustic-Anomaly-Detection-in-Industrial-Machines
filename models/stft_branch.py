0038927f4d1267da174255e66e89997d1dc272ca
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-08-08 13:16:59 +0530

    V1.0 Refactored the code to support the feature extraction from the cqt features

--- models/stft_branch.py
+++ models/stft_branch.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from torchvision.models import resnet18
+from timm import create_model
 
 class STFTFeatureExtractor(nn.Module):
     def __init__(self):
@@ -25,7 +26,8 @@ class STFTFeatureExtractor(nn.Module):
         x = self.layer4(x)
 
         return x
-    
+
+# STFT Feature Extractor with Novalty planned Frequency adaptive convolution
 class STFTFrequencyAdaptiveFeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
@@ -60,4 +62,25 @@ class STFTFrequencyAdaptiveFeatureExtractor(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
+        return x
+
+# CQT Feature Extractor using the mobilevit_xxs model
+class CQTFeatureExtractor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = create_model('mobilevit_xxs', pretrained=False,num_classes=0, global_pool='')
+
+    def forward(self,x):
+        return self.model(x)
+
+#Projection + Polling Block to Match the Channels, Height, and Width of the Extractor Features to Match with the Shape(4,16) with 256 Channel Size
+class FeatureProjector(nn.Module):
+    def __init__(self, in_channels, out_channels=256, target_hw=(4,16)):
+        super().__init__()
+        self.proj = nn.Conv2d(in_channels, out_channels=256, kernel_size=1)
+        self.pool = nn.AdaptiveAvgPool2d(target_hw)
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = self.pool(x)
         return x
