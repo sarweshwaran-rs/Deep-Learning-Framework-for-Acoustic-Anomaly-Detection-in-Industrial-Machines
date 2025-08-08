fcb3d7c94e2f09d602b2dd13528ecb49050f9815
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-08-08 19:37:18 +0530

    V1.0 Refactored the cafm module to assert the input shape, removed the unsed head parameter and make it as single head

--- models/cafm.py
+++ models/cafm.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 
 class CAFM(nn.Module):
-    def __init__(self, dim=256, heads = 4):
+    def __init__(self, dim=256):
         super().__init__()
         self.wq1= nn.Linear(dim, dim)
         self.wq2 = nn.Linear(dim, dim)
@@ -21,22 +21,24 @@ class CAFM(nn.Module):
 
     def forward(self, stft_feat, cqt_feat):
         B, C, H, W = stft_feat.shape
+        assert stft_feat.shape == cqt_feat.shape, "Shape mismatch between STFT and CQT features"
 
-        stft_seq = stft_feat.flatten(2).transpose(1, 2)
-        cqt_seq = cqt_feat.flatten(2).transpose(1, 2)
+        stft_seq = stft_feat.flatten(2).transpose(1, 2) # [B, N, C]
+        cqt_seq = cqt_feat.flatten(2).transpose(1, 2) # [B, N, C]
 
         # STFT attends to CQT
         Q1 = self.wq1(stft_seq)
         K1 = self.wq2(cqt_seq)
         V1 = self.wq3(cqt_seq)
-        attention_1 = self.softmax(torch.bmm(Q1, K1.transpose(1, 2)) / (C ** 0.5))
+        dk = Q1.size(-1)
+        attention_1 = self.softmax(torch.bmm(Q1, K1.transpose(1, 2)) / (dk ** 0.5))
         out_1 = torch.bmm(attention_1, V1)
 
         # CQT attends to STFT
         Q2 = self.wq4(cqt_seq)
         K2 = self.wq5(stft_seq)
         V2 = self.wq6(stft_seq)
-        attention_2 = self.softmax(torch.bmm(Q2, K2.transpose(1, 2)) / (C ** 0.5))
+        attention_2 = self.softmax(torch.bmm(Q2, K2.transpose(1, 2)) / (dk ** 0.5))
         out_2 = torch.bmm(attention_2, V2)
 
         # Mean pool and Fuse
