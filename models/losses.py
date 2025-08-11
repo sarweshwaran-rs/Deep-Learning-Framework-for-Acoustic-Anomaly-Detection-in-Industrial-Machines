e536f25af71e3b803ff6b27d960fe212c6c92cbb
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-08-11 11:39:57 +0530

    V1.0 Refactored the losses to produce the loss correctly

--- models/losses.py
+++ models/losses.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=1.0):
+    def __init__(self, margin=0.5):
+        """
+        margin: separation in cosine space (0 to 2).
+        e.g., marging = 0.5 means anomalies should have similarity <= 1 - margin = 0.5
+        """
         super().__init__()
         self.margin = margin
 
@@ -12,10 +17,20 @@ class ContrastiveLoss(nn.Module):
         prototype: (D, ) or (1, D)
         labels: tensor(B, ) with 0=normal, 1=anomaly
         """
+        #Normalize for cosine space
+        embeddings = F.normalize(embeddings, dim=1)
+        prototype = F.normalize(prototype,dim=0)
+
         if prototype.dim() == 1:
             prototype = prototype.unsqueeze(0)
-        prototype = prototype.expand(embeddings.size(0), prototype.size(1)) #(B,D)
-        distances = torch.norm(embeddings - prototype, p=2, dim=1) #(B,)
-        labels = labels.float()
-        loss = (1.0 - labels) * distances.pow(2) + labels * torch.clamp(self.margin - distances, min=0.0).pow(2)
-        return loss.mean()
+        prototype = prototype.expand(embeddings.size(0), -1) #(B,D)
+        
+        cosine_sim = torch.sum(embeddings * prototype, dim=1)
+
+        pos_mask = (labels == 0).float()
+        neg_mask = (labels == 1).float()
+
+        # Loss Terms
+        pos_loss = ((1 - cosine_sim) * pos_mask).sum() / pos_mask.sum().clamp(min=1) # pull normals toward prototype
+        neg_loss = (torch.clamp(cosine_sim - (1 - self.margin), min=0) * neg_mask).sum() / neg_mask.sum().clamp(min=1) # Push anomalies away
+        return (pos_loss + neg_loss)
